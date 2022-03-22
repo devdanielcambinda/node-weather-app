c714c35d4d328cef8f20fc1ce66757efc5f8cfab
Daniel Cambinda <danielcambinda@gmail.com>
2022-03-22 17:45:04 +0000

    finished version

---
+++ src/app.js
@@ -0,0 +1,118 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+
+const forecast = require("./utils/forecast");
+const geocode = require("./utils/geocode");
+
+const app = express()
+const port = process.env.PORT || 3000
+
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname,'../public',)
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('/', (req,res)=>{
+    res.render('index',{
+        title: ' Weather App',
+        name: 'Daniel Cambinda'
+    })
+})
+
+app.get('/about', (req,res) =>{
+    res.render('about',{
+        title: 'About Me',
+        name: 'Daniel Cambinda'
+    })
+})
+
+app.get('/help', (req,res) =>{
+    res.render('help',{
+        title: 'Help',
+        name: 'Daniel Cambinda',
+        helpText: 'This is some helful text.'
+    })
+})
+
+app.get('/weather', (req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide an address.'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({error})
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        //latitude, longitude
+
+        if (error) {
+          return res.send({error})
+        }
+
+        res.send({
+            forecast: forecastData,
+            location,
+            address: req.query.address
+        })
+
+      });
+    });
+
+    /* res.send({
+      forecast: "It is snowing",
+      location: "Philadelphia",
+      address: req.query.address,
+    }); */
+})
+
+
+app.get('/products', (req,res)=>{
+
+    if(!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+
+})
+
+//404 page must be the last one because the request iterates every route
+
+app.get('/help/*',(req,res)=>{
+    res.render("404", {
+      title: "404 Error",
+      name: "Daniel Cambinda",
+      errorMessage: "Help article not found.",
+    });
+})
+
+app.get('*', (req,res) =>{
+    res.render('404',{
+        title: '404 Error',
+        name: 'Daniel Cambinda',
+        errorMessage: 'Page not found.'
+    })
+})
+
+app.listen(port, () =>{
+    console.log(`Server is up on port ${port}.`)
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,33 @@
+const request = require('request')
+require("dotenv").config({ path: __dirname + "/./../../.env" });
+
+const forecast = (latitude,longitude,callback) => {
+  const url =
+    `http://api.weatherstack.com/current?access_key=${encodeURIComponent(process.env.TOKEN_WEATHERSTACK)}&query=${encodeURIComponent(latitude)},${encodeURIComponent(longitude)}&units=m`;
+
+    /*
+    units=s for kelvin
+    units=m for Celsius
+    units=f for fahrenheit
+
+    latitude, longitude 
+
+    */
+  request({url, json: true}, (error,{body} = {}) => {
+
+    // const data  = JSON.parse(response.body)
+
+    if (error){
+        callback("Unable to connect to weather services!",undefined)
+    }else if(body.error) {
+        callback("Unable to find location",undefined)
+    } else {
+        callback(undefined,`${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%.`
+        );
+    }
+        
+   
+})
+}
+
+module.exports = forecast
